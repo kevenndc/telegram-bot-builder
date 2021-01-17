670112771b5455abdce9718a0ae44a87a4366e4b
Kevenn <k.douglascaterinque@gmail.com>
2021-01-17 12:47:29 -0300

    minor changes

--- app.ts
+++ app.ts
@@ -1,26 +1,38 @@
 import "https://deno.land/x/dotenv/load.ts";
 import {
+    BotCommand,
   Message,
   TelegramBot,
   UpdateType,
 } from "https://deno.land/x/telegram_bot_api/mod.ts";
-
 import messages from "./messages.ts";
 
-//const { messages } = messagesJSON;
 const TOKEN: string = <string> Deno.env.get("BOT_TOKEN");
 const bot = new TelegramBot(TOKEN);
-
 
 
 bot.on(UpdateType.Message, async ({ message }) => {
     if (isStart(message)) {
         const username = message.chat.first_name;
-        const variables = { username: username };
+        const variables = new Map([['username', username]]);
         await bot.sendMessage({
             chat_id: message.chat.id,
             text: buildText(messages.welcome, variables),
         });
+        await bot.setMyCommands({
+            commands: [
+                {
+                    command: 'see_menu',
+                    description: 'Mostra o cardápio para o usuário.'
+                },
+                {
+                    command: 'place_order',
+                    description: 'Inicia o pedido do usuário'
+                }
+            ]
+        });
+    } else {
+        const expectedCommands =  await bot.getMyCommands();
     }
 });
 
@@ -34,9 +46,24 @@ function isStart(msg: Message): boolean {
   return false;
 }
 
-function buildText(template: string, variables: Object): string {
+function isExpectedCommand(msg: Message, commands: BotCommand[]): boolean {
+    commands.forEach(({command, description}) => {
+        if (msg.text === description) {
+            return true;
+        }
+    });
+    return false;
+}
+
+/**
+ * 
+ * @param {string} template - The message template template string. 
+ * @param {Map<string, any>} variables - The Map containing all variables (key) that should be replaced with its resprctive values on the template.
+ * 
+ */
+function buildText(template: string, variables: Map<string, any>): string {
     let output: string = template;
-    for (const [key, value] of Object.entries(variables)) {
+    for (const [key, value] of variables) {
         output = output.replaceAll(`%{${key}}%`, <string> value);
     }
     return output;
--- messages.ts
+++ messages.ts
@@ -1,5 +1,5 @@
 const messages = {
-    welcome: "Olá %{username}%",
+    welcome: "Olá %{username}%\nSeja bem-vindo ao assistente virtual da nossa loja.\nAqui você poderá fazer o seu pedido de forma rápida.\nO que você deseja fazer?",
 }
 
 export default messages;
